70626a736bb22d93f5119c0dd08cb841502234d0
Sonia Keys <skeys.cfa.harvard.edu>
2014-11-21 19:48:00 +0000

    polish API

--- exit.go
+++ exit.go
@@ -40,66 +40,92 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-type fatal struct {
-	code int
-	msg  string
+// Exit specifies an exit code and logger for logging a final message.
+//
+// If Logger is nil, the log package default logger is used.
+type Exit struct {
+	ExitCode int
+	*log.Logger
+}
+
+// Default holds Exit setting for the Log and Logf functions.
+var Default = Exit{ExitCode: 1}
+
+type msg struct {
+	Exit
+	string
 }
 
 // Handler is the fatal error handler.  Defer at the start of main() before
-// calling any of Code, Log, Logf, CodeLog, or CodeLogf.
+// calling any of Code, Log, Logf, Exit.Log, or Exit.Logf.
 //
-// Handler handles paincs originating from these functions by logging messages
-// with the standard logger of the log package and then exiting
-// with an exit code.
+// When Handler is deferred first, Go ensures that all other deferred functions
+// are run first.  Handler then handles paincs originating from Code, Log,
+// Logf, Exit.Log, or Exit.Logf by logging a message and then exiting with
+// an exit code.
 //
-// By default, output of the standard logger goes to stderr.
-// If stderr is going to a terminal, Handler first sets logging flags to 0
-// to suppress the date and time stamp.  In the case where stderr is
-// redirected to a file for example, Handler does not reset the flags.
+// There are a couple of frills.  Empty messages are not logged; the program
+// simply exits with the specifed code.  With non-empty messages, if a logger
+// is not specified and stderr seems to be directed to a terminal, Handler sets
+// logging flags 0 to suppress the usual date and time stamp.
 func Handler() {
 	err := recover()
 	if err == nil {
 		return
 	}
-	f, ok := err.(fatal)
+	m, ok := err.(msg)
 	if !ok {
 		panic(err)
 	}
-	if f.msg > "\n" {
-		if terminal.IsTerminal(int(os.Stderr.Fd())) {
+	if m.string > "\n" {
+		switch {
+		case m.Logger != nil:
+			m.Logger.Print(m.string)
+		case terminal.IsTerminal(int(os.Stderr.Fd())):
 			log.SetFlags(0)
+			fallthrough
+		default:
+			log.Print(m.string)
 		}
-		log.Print(f.msg)
 	}
-	os.Exit(f.code)
+	os.Exit(m.ExitCode)
 }
 
-// Code calls any deferred functions, then exits the program with the given
-// exit code.
-func Code(code int) {
-	panic(fatal{1, ""})
-}
-
-// CodeLog calls any deferred functions, logs a message as with log.Println,
-// then exits the program with the given exit code.
-func CodeLog(code int, logArgs ...interface{}) {
-	panic(fatal{code, fmt.Sprintln(logArgs...)})
-}
-
-// CodeLogf calls any deferred functions, logs a message as with log.Printf,
-// then exits the program with the given exit code.
-func CodeLogf(code int, logFormat string, logArgs ...interface{}) {
-	panic(fatal{code, fmt.Sprintf(logFormat, logArgs...)})
-}
-
-// Log calls any deferred functions, logs a message as with log.Println,
-// then exits the program with an exit code of 1.
+// Log runs any deferred functions, logs a message as with log.Println,
+// then exits the program with an exit code.
+//
+// The logger and exit code used are those of exit.Default.
 func Log(args ...interface{}) {
-	panic(fatal{1, fmt.Sprintln(args...)})
+	panic(msg{Default, fmt.Sprintln(args...)})
 }
 
 // Logf calls any deferred functions, logs a message as with log.Printf,
-// then exits the program with an exit code of 1.
+// then exits the program with an exit code.
+//
+// The logger and exit code used are those of exit.Default.
 func Logf(format string, args ...interface{}) {
-	panic(fatal{1, fmt.Sprintf(format, args...)})
+	panic(msg{Default, fmt.Sprintf(format, args...)})
+}
+
+// Code runs any deferred functions, then exits the program with the given
+// exit code.
+func Code(code int) {
+	Default.ExitCode = code
+	panic(msg{Default, ""})
+}
+
+// Log runs any deferred functions, logs a message as with log.Println,
+// then exits the program with an exit code.
+//
+// The logger and exit code used are those of the receiver x.
+func (x Exit) Log(args ...interface{}) {
+	panic(msg{x, fmt.Sprintln(args...)})
+}
+
+// Logf calls any deferred functions, logs a message as with log.Printf,
+// then exits the program with an exit code.
+//
+// The logger and exit code used are those of the receiver x.
+func (x Exit) Logf(format string, args ...interface{}) {
+	panic(msg{x, fmt.Sprintf(format, args...)})
 }
